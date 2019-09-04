70ca5c20b94587d2dda4649db84b19fbf52a68f7
riverbo <riverbo@126.com>
2019-09-04 11:16:23 +0800

    modified member car

--- ecarpo-bms-imp-server/ecarpo-bms-imp-server-provider/src/main/java/com/ecarpo/bms/eas/server/crmmembercar/service/CrmMemberCarManager.java
+++ ecarpo-bms-imp-server/ecarpo-bms-imp-server-provider/src/main/java/com/ecarpo/bms/eas/server/crmmembercar/service/CrmMemberCarManager.java
@@ -4,6 +4,7 @@
  */
 package com.ecarpo.bms.eas.server.crmmembercar.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -37,37 +38,44 @@ public class CrmMemberCarManager extends BaseManager<CrmMemberCarMapper, CrmMemb
     int n = 0;
     CrmCustomDO c = customManager.selectByMobile(dto.getMobile(), dto.getStore_id());
     if (c == null) {
+      log.info("customer(mobile-{}, storeId-{}) none exists, must be add cust. ", dto.getMobile(), dto.getStore_id());
       c = DAOUtils.cloneBean(CrmCustomDO.class, dto);
-      if (dto.getMember_name() != null) {
+      if (StringUtils.isNoneBlank(dto.getMember_name())) {
         Integer memberTypeId = memberStoreManager.selectMemberIdByMemberName(dto.getMember_name(),
             dto.getStore_id());
         c.setLevel(memberTypeId);
       }
       n = customManager.insertSelective(c);
     } else {
-      log.info("customer({}, {}) is exists. ", c.getName(), c.getId());
+      log.info("customer(mobile-{}, storeId-{}, custName-{}, custId-{}) is exists. ", 
+          dto.getMobile(), dto.getStore_id(), c.getName(), c.getId());
     }
     // 判断此人是否为会员
-    if (dto.getMember_name() != null) {
+    if (StringUtils.isNoneBlank(dto.getMember_name())) {
+      log.info("memeber(level-{}) none blank, must be process member. ", dto.getMember_name());
       Long l = memberStoreManager.existsByCustId(c.getId());
       if (l == null) {
+        log.info("member(custName-{}, custId-{}) none exists, must be add it. ", c.getName(), c.getId());
         CrmMemberStoreDO u = DAOUtils.cloneBean(CrmMemberStoreDO.class, dto);
         //DAOUtils.cloneBean(u, c);
         u.setCstaff(dto.getCreatorId().intValue());
         u.setCustom_id(c.getId());
         n += memberStoreManager.insertSelective(u);
       } else {
-        log.info("member({}, {}) is exists. ", c.getName(), c.getId());
+        log.info("member(custName-{}, custId-{}) exists, could not be add cust to member. ", c.getName(), c.getId());
       }
     } else {
-      log.info("memeber level is null, couldn't add member. ");
+      log.info("memeber (level-{}) is blank, skip it(could not be add member). ", dto.getMember_name());
     }
-    if (this.existsByPlateNo(dto.getPlate_no(), dto.getStore_id()) != null) {
-      log.info("plate_no({}) is exists, couldn't add car. ", dto.getPlate_no());
-    } else {
+    //
+    Long l = this.existsByPlateNo(dto.getPlate_no(), dto.getStore_id());
+    if (l == null) {
+      log.info("memberCar(plate_no-{}, storeId-{}) none exists, must be add car. ", dto.getPlate_no(), dto.getStore_id());
       CrmMemberCarDO memberCar = DAOUtils.cloneBean(CrmMemberCarDO.class, dto);
       memberCar.setCustom_id(c.getId());
       n += super.insertSelective(memberCar);
+    } else {
+      log.info("memberCar(plate_no-{}, storeId-{}) exists, could not be add car. ", dto.getPlate_no(), dto.getStore_id());
     }
     return n;
   }
@@ -76,5 +84,4 @@ public class CrmMemberCarManager extends BaseManager<CrmMemberCarMapper, CrmMemb
     return mapper.existsByPlateNo(plateNo, store_id);
   }
 
-  
 }
